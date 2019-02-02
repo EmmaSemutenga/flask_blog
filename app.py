61efe9f00971ffee65555324e9e76a952c62ee60
Emmanuel Semutenga <emzsema@gmail.com>
2019-02-02 23:47:58 +0300

    initial setup

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Kitengejja',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template('home.html', posts=posts)
