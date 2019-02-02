bd88afb5b9d849c7f3fec793c711202793560ca0
Emmanuel Semutenga <emzsema@gmail.com>
2019-02-03 01:10:10 +0300

    added forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "getYours"
 
 posts = [
     {
@@ -20,3 +23,23 @@ posts = [
 @app.route("/")
 def home():
     return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}.', 'success')#second parameter is the category of the message
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login",  methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login was successful.', 'success')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
