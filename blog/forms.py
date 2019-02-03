624251323f0e9bca8781018c29ed049961a39741
Emmanuel Semutenga <emzsema@gmail.com>
2019-02-03 16:03:25 +0300

    added user authentication

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])#first paramenter will also act as the label
@@ -8,6 +9,17 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    #to catch errors during registration
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Usename is already taken. Please choose another one')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('Email is already taken. Please choose another one')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from blog.forms import RegistrationForm, LoginForm
 from blog.models import User, Post
-from blog import app
+from blog import app, bcrypt, db
+from flask_login import login_user, current_user, logout_user, login_required
+
 posts = [
     {
         'author': 'Corey Schafer',
@@ -27,16 +29,46 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}.', 'success')#second parameter is the category of the message
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created, you can now login.', 'success')#second parameter is the category of the message
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login",  methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login was successful.', 'success')
-        return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('Login was successful.', 'success')
+            next_page = request.args.get('next') # args is a dictionary but accessing it with request['next'] will produce keyvalue error if key doesn't exist
+            if next_page:                  
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+            #shorthand
+            #return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login was not successful.', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
